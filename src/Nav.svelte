bdd079b902d73759904fa86675f5d7d8f187fa57
Allen Hai <allen.hai@hotmail.com>
2019-12-15 05:36:23 +0000

    add nav w/ auth inputs

---
+++ src/Nav.svelte
@@ -0,0 +1,35 @@
+<script>
+  let loggedIn = false;
+
+  function signUp(event) {
+    event.preventDefault();
+    loggedIn = true;
+  }
+  function logIn(event) {
+    event.preventDefault();
+    loggedIn = true;
+  }
+  function logOut(event) {
+    event.preventDefault();
+    loggedIn = false;
+  }
+</script>
+
+<nav>
+  <form>
+    {#if !loggedIn}
+      <label for="username">
+        <span>username:</span>
+        <input id="username" type="text" />
+      </label>
+      <label for="password">
+        <span>password:</span>
+        <input id="password" type="password" />
+      </label>
+      <button type="submit" on:click={signUp}>sign up</button>
+      <button type="submit" on:click={logIn}>login</button>
+    {:else}
+      <button type="submit" on:click={logOut}>logout</button>
+    {/if}
+  </form>
+</nav>
